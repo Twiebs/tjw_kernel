d80fa736ee87310d9e44f462a323bb59395cd349
Torin Wiebelt <torinwiebelt@gmail.com>
2020-01-29 01:57:48 -0500

    removed the declaration for kdebug_log_virtual_address_info_2MB. It is never defined or used.

--- src/kernel_debug.h
+++ src/kernel_debug.h
@@ -1,5 +1,4 @@
 static void kdebug_log_page_table_entry_info(const uintptr_t entry);
-static void kdebug_log_virtual_address_info_2MB(const uintptr_t virtual_address);
 
 static void kdebug_log_register_state(Register_State *state);
 static void kdebug_log_interrupt_stack_frame(Interrupt_Stack_Frame *stack_frame);
