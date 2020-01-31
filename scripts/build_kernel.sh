81dddddc46d7024a35244e4d3bb81862d686594b
Torin Wiebelt <torinwiebelt@gmail.com>
2020-01-30 21:39:46 -0500

    I renamed build.c to single_translation_unit_build and moved it into the src directory. I also bumped the version number to make sure the build is still working.

--- scripts/build_kernel.sh
+++ scripts/build_kernel.sh
@@ -7,9 +7,9 @@ COMPILER_FLAGS="-g -O0"
 
 nasm -fbin $KERNEL_SOURCE_DIRECTORY/secondary_cpu_init.asm -o $BINARY_OUTPUT_DIRECTORY/trampoline.bin 
 $BINARY_OUTPUT_DIRECTORY/bin_to_txt $BINARY_OUTPUT_DIRECTORY/trampoline.bin $KERNEL_SOURCE_DIRECTORY/trampoline.txt
-gcc $COMPILER_FLAGS -m64 -mno-red-zone -nostdlib -ffreestanding -fno-stack-protector -I $KERNEL_SOURCE_DIRECTORY -c $KERNEL_SOURCE_DIRECTORY/../build.c -o $BINARY_OUTPUT_DIRECTORY/build.c.o
+gcc $COMPILER_FLAGS -m64 -mno-red-zone -nostdlib -ffreestanding -fno-stack-protector -I $KERNEL_SOURCE_DIRECTORY -c $KERNEL_SOURCE_DIRECTORY/single_translation_unit_build.c -o $BINARY_OUTPUT_DIRECTORY/single_translation_unit_build.c.o
 nasm -felf64 $KERNEL_SOURCE_DIRECTORY/primary_cpu_init.asm -i$KERNEL_SOURCE_DIRECTORY -o $BINARY_OUTPUT_DIRECTORY/primary_cpu_init.asm.o
 nasm -felf64 $KERNEL_SOURCE_DIRECTORY/x86_64_entry.asm -i$KERNEL_SOURCE_DIRECTORY -o $BINARY_OUTPUT_DIRECTORY/x86_64_entry.asm.o
-ld -melf_x86_64 -n -T $KERNEL_SOURCE_DIRECTORY/../kernel_link.ld -o $BINARY_OUTPUT_DIRECTORY/kernel $BINARY_OUTPUT_DIRECTORY/build.c.o $BINARY_OUTPUT_DIRECTORY/x86_64_entry.asm.o $BINARY_OUTPUT_DIRECTORY/primary_cpu_init.asm.o
+ld -melf_x86_64 -n -T $KERNEL_SOURCE_DIRECTORY/../kernel_link.ld -o $BINARY_OUTPUT_DIRECTORY/kernel $BINARY_OUTPUT_DIRECTORY/single_translation_unit_build.c.o $BINARY_OUTPUT_DIRECTORY/x86_64_entry.asm.o $BINARY_OUTPUT_DIRECTORY/primary_cpu_init.asm.o
 objcopy --only-keep-debug $BINARY_OUTPUT_DIRECTORY/kernel $BINARY_OUTPUT_DIRECTORY/debug_symbols
 objcopy --strip-debug $BINARY_OUTPUT_DIRECTORY/kernel $BINARY_OUTPUT_DIRECTORY/kernel
---
+++ src/single_translation_unit_build.c
@@ -0,0 +1,129 @@
+#define BUILD_DEBUG
+#define FORCE_REDRAW_ON_LOG_ENTRY 1
+
+#pragma GCC diagnostic error "-Wimplicit-function-declaration"
+#pragma GCC diagnostic error "-Wshadow"
+#pragma GCC diagnostic warning "-Wall"
+#pragma GCC diagnostic warning "-Wextra"
+
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wunused-function"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wunused-local-typedefs"
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+#pragma GCC diagnostic ignored "-Wunused-but-set-parameter"
+
+#ifdef BUILD_DEBUG 
+#define COMPILER_FLAGS -g -O0 
+#else//BUILD_DEBUG
+#define COMPILER_FLAGS -g -O2
+#endif//BUILD_DEBUG
+
+#include <stdint.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+//Utilities
+#include "utils/error_code.h"
+#include "utils/math.h"
+#include "utils/memory.h"
+#include "utils/string.h"
+#include "utils/persistent_block_allocator.h"
+
+
+//Main System
+#include "tasking/lock.h"
+
+#include "kernel_pci.h"
+#include "kernel_memory.h"
+#include "system/descriptor_tables.h"
+#include "system.h"
+#include "kernel.h"
+#include "assembly.h"
+#include "hardware_keyboard.h"
+#include "kernel_log.h"
+
+
+#include "elf64.h"	
+#include "system/interrupt.h"
+
+//Shell
+#include "shell/command_line_shell.h"
+#include "shell/shell_commands.h"
+
+//Filesystem
+#include "filesystem/virtual_filesystem.h"
+#include "filesystem/storage_device.h"
+#include "filesystem/ext2_filesystem.h"
+
+#include "utils/buffered_file_reader.h"
+
+#include "filesystem/ext2_debug.h"
+
+//Graphics
+#include "graphics/vga_mode_3.h"
+#include "graphics/display.h"
+#include "graphics/software_renderer.h"
+#include "graphics/graphics_device.h"
+#include "graphics/bochs_graphics_device.h"
+#include "graphics/intel.h"
+
+//Desktop
+#include "desktop_enviroment/desktop_enviroment.h"
+#include "tasking/elf64_loader.h" 
+
+//USB devices and drivers
+#include "usb/usb.h"
+#include "usb/ehci.h"
+#include "usb/usb_device.h"
+
+//Debug Procedures
+#include "kernel_debug.h"
+#include "memory_debug.h"
+#include "graphics/display_debug.h"
+
+//  Source Files
+//===========================
+
+//System
+#include "kernel_entry.c"
+#include "system/interrupt.c"
+#include "kernel_log.c"
+#include "system.c" 
+#include "kernel_memory.c"
+
+//Utilities Source Files
+#include "utils/math.c"
+#include "utils/memory.c"
+#include "utils/string.c"
+#include "utils/persistent_block_allocator.c"
+#include "utils/buffered_file_reader.c"
+
+//Filesystem Source files
+#include "filesystem/virtual_filesystem.c"
+#include "filesystem/ext2_filesystem.c"
+#include "filesystem/storage_device.c"
+
+//Shell Source files
+#include "shell/command_line_shell.c"
+#include "shell/shell_commands.c"
+
+//Graphics Source files
+#include "graphics/vga_mode_3.c"
+#include "graphics/display.c"
+#include "graphics/software_renderer.c"
+#include "graphics/bochs_graphics_device.c"
+#include "graphics/intel.c"
+
+//Desktop
+#include "desktop_enviroment/desktop_enviroment.c"
+#include "tasking/elf64_loader.c"
+
+//USB
+#include "usb/usb_protocol.c"
+#include "usb/ehci.c"
+
+//Debug
+#include "graphics/display_debug.c"
+#include "kernel_memory_debug.c"
