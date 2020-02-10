c968789e94045842a699dc55bc69ee0b7f5e04a3
Torin Wiebelt <torinwiebelt@gmail.com>
2020-02-09 22:57:08 -0500

    added more fields and debug logging for the hba_capabilities

--- code/kernel/ahci.c
+++ code/kernel/ahci.c
@@ -1,7 +1,23 @@
 
+void debug_log_AHCI_HBA_Capabilities(volatile AHCI_HBA_Capabilities *hba_capabilities)
+{
+    log_debug(AHCI, "hba_capabilities:");
+    log_debug(AHCI, "  number_of_ports: %u", (uint32_t)hba_capabilities->number_of_ports);
+    log_debug(AHCI, "  supports_external_sata: %u", (uint32_t)hba_capabilities->supports_external_sata);
+    log_debug(AHCI, "  enclosure_management_supported: %u", (uint32_t)hba_capabilities->enclosure_management_supported);
+    log_debug(AHCI, "  command_completion_coalescing_supported: %u", (uint32_t)hba_capabilities->command_completion_coalescing_supported);
+    log_debug(AHCI, "  number_of_command_slots: %u", (uint32_t)hba_capabilities->number_of_command_slots);
+}
+
 Error_Code ahci_initalize(PCI_Device *pci_device)
 {
-    log_info(EHCI, "Initializing AHCI - Advanced Host Controller Interface (SATA)");
+    log_info(AHCI, "Initializing AHCI - Advanced Host Controller Interface (SATA)");
+
+    const Physical_Address registers_physical_address =  pci_device_get_base_address_0(pci_device);
+    const Virtual_Address registers_virtual_address = memory_map_physical_mmio(registers_physical_address, 1);
+
+    volatile AHCI_HBA_Capabilities *hba_capabilities = (volatile AHCI_HBA_Capabilities *)registers_virtual_address;
+    debug_log_AHCI_HBA_Capabilities(hba_capabilities);
 
     return Error_Code_NONE;
 }
--- code/kernel/ahci.h
+++ code/kernel/ahci.h
@@ -2,11 +2,14 @@
 
 typedef struct
 {
-    union 
-    {
+    union {
         struct
         {
-            uint32_t number_of_ports : 4; // 0-4
+            uint32_t number_of_ports : 4;                         // 0-4
+            uint32_t supports_external_sata : 1;                  // 5
+            uint32_t enclosure_management_supported : 1;          // 6
+            uint32_t command_completion_coalescing_supported : 1; // 7
+            uint32_t number_of_command_slots : 4;                 // 8 –12
         };
 
         uint32_t value;
