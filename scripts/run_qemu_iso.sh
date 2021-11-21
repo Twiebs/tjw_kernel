404eff1c41188bfed87c6fa4d2dffe8273e5304b
Torin Wiebelt <torinwiebelt@gmail.com>
2021-11-21 17:00:59 -0500

    Updated run_qemu_iso.sh so that it no longer uses a hard drive disk image

--- scripts/run_qemu_iso.sh
+++ scripts/run_qemu_iso.sh
@@ -2,5 +2,4 @@
 source $(dirname $0)/variables.sh
 
 $QEMU_EXECUTABLE -boot d -cdrom $IMAGE_OUTPUT_DIRECTORY/kernel.iso \
--hda $IMAGE_OUTPUT_DIRECTORY/disk.img \
 -smp $QEMU_CPU_CORE_COUNT -m $QEMU_MEMORY_IN_MB -serial file:$LOG_OUTPUT_DIRECTORY/serial.txt
