496774792ed0af016e6e054cc4235b97ff5e4e9b
Torin Wiebelt <torinwiebelt@gmail.com>
2021-11-21 18:25:43 -0500

    Added hard drive support for the ISO script

--- scripts/run_qemu_iso.sh
+++ scripts/run_qemu_iso.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 source $(dirname $0)/variables.sh
 
-$QEMU_EXECUTABLE -boot d -cdrom $IMAGE_OUTPUT_DIRECTORY/kernel.iso \
+$QEMU_EXECUTABLE -boot d \
+-cdrom $IMAGE_OUTPUT_DIRECTORY/kernel.iso \
+-drive id=disk,format=raw,file=$IMAGE_OUTPUT_DIRECTORY/hdd.img,if=none \
+-device ahci,id=ahci \
+-device ide-hd,drive=disk,bus=ahci.0 \
 -smp $QEMU_CPU_CORE_COUNT -m $QEMU_MEMORY_IN_MB -serial file:$LOG_OUTPUT_DIRECTORY/serial.txt
